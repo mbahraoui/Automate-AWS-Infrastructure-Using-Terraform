8028e95d462b527846379643b19d83805f54d441
mohamedbh <mohamedbahraoui20@gmail.com>
2023-09-19 21:29:07 +0100

    use main route table

--- main.tf
+++ main.tf
@@ -23,27 +23,48 @@ resource "aws_subnet" "myapp-subnet-1" {
     }
 }
 
-resource "aws_route_table" "myapp-route-table" {
-    vpc_id = aws_vpc.myapp-vpc.id
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.myapp-igw.id
-    }
+# resource "aws_route_table" "myapp-route-table" {
+#   vpc_id = aws_vpc.myapp-vpc.id
 
-    tags = {
-      Name : "${var.env_prefix}-rtb"
-    }
-}
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.myapp-igw.id
+#   }
+
+#   tags = {
+#     Name : "${var.env_prefix}-rtb"
+#   }
+# }
+
+# resource "aws_internet_gateway" "myapp-igw" {
+#   vpc_id = aws_vpc.myapp-vpc.id
+#   tags = {
+#     Name : "${var.env_prefix}-igw"
+#   }
+# }
+
+# resource "aws_route_table_association" "a-rtb-subnet" {
+#   subnet_id = aws_subnet.myapp-subnet-1.id
+#   route_table_id = aws_route_table.myapp-route-table.id
+# }
+
+# Use Main Route Table
 
 resource "aws_internet_gateway" "myapp-igw" {
-    vpc_id = aws_vpc.myapp-vpc.id
-    tags = {
-      Name : "${var.env_prefix}-igw"
-    }
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags = {
+    Name : "${var.env_prefix}-igw"
+  }
 }
 
-resource "aws_route_table_association" "a-rt-subnet" {
-    subnet_id = aws_subnet.myapp-subnet-1.id
-    route_table_id = aws_route_table.myapp-route-table.id
-}
+resource "aws_default_route_table" "main_rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
 
+  tags = {
+    Name : "${var.env_prefix}-main-rtb"
+  }
+}
