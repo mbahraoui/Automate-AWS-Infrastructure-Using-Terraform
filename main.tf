db316c28a8eb53de981a0327c0aa35490430b839
mohamedbh <mohamedbahraoui20@gmail.com>
2023-09-20 10:42:51 +0100

    fetch ami id

--- main.tf
+++ main.tf
@@ -101,3 +101,26 @@ resource "aws_default_security_group" "default-sg" {
   }
 }
 
+# fetch AMI 
+
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+output "aws_ami_id" {
+  value = data.aws_ami.latest-amazon-linux-image.id
+}
+
+resource "aws_instance" "myapp-server" {
+  ami = data.aws_ami.latest-amazon-linux-image.id
+}
+
