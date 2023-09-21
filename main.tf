c5810f17156d55419b50fc403ad8dd6b77fca72c
mohamedbh <mohamedbahraoui20@gmail.com>
2023-09-21 11:59:40 +0100

    modularize tf project

--- main.tf
+++ main.tf
@@ -2,14 +2,6 @@ provider "aws" {
     region = "eu-west-3"
 }
 
-variable vpc_cidr_block {}
-variable subnet_cidr_block {}
-variable avail_zone {}
-variable env_prefix {}
-variable my_ip {}
-variable instance_type {}
-variable public_key_location {}
-
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
     tags = {
@@ -17,134 +9,26 @@ resource "aws_vpc" "myapp-vpc" {
     }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
-    vpc_id = aws_vpc.myapp-vpc.id
-    cidr_block = var.subnet_cidr_block
-    availability_zone = var.avail_zone
-    tags = {
-      Name : "${var.env_prefix}-subnet-1"
-    }
-}
-
-# resource "aws_route_table" "myapp-route-table" {
-#   vpc_id = aws_vpc.myapp-vpc.id
-
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.myapp-igw.id
-#   }
-
-#   tags = {
-#     Name : "${var.env_prefix}-rtb"
-#   }
-# }
-
-# resource "aws_internet_gateway" "myapp-igw" {
-#   vpc_id = aws_vpc.myapp-vpc.id
-#   tags = {
-#     Name : "${var.env_prefix}-igw"
-#   }
-# }
-
-# resource "aws_route_table_association" "a-rtb-subnet" {
-#   subnet_id = aws_subnet.myapp-subnet-1.id
-#   route_table_id = aws_route_table.myapp-route-table.id
-# }
-
-# Use Main Route Table
-
-resource "aws_internet_gateway" "myapp-igw" {
+module "myapp-subnet" {
+  source = "./modules/subnet"
+  subnet_cidr_block = var.subnet_cidr_block
+  avail_zone= var.avail_zone
+  env_prefix= var.env_prefix
   vpc_id = aws_vpc.myapp-vpc.id
-  tags = {
-    Name : "${var.env_prefix}-igw"
-  }
-}
-
-resource "aws_default_route_table" "main_rtb" {
   default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id
-  }
-
-  tags = {
-    Name : "${var.env_prefix}-main-rtb"
-  }
 }
 
-# create default SG in our vpc
-resource "aws_default_security_group" "default-sg" {
+module "myapp-webserver" {
+  source = "./modules/webserver"
   vpc_id = aws_vpc.myapp-vpc.id
-
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = [var.my_ip]
-  }
-
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    prefix_list_ids = []
-  }
-
-  tags = {
-    Name : "${var.env_prefix}-default-sg"
-  }
-}
-
-# fetch AMI 
-
-data "aws_ami" "latest-amazon-linux-image" {
-  most_recent = true
-  owners = ["amazon"]
-  filter {
-    name = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-  filter {
-    name = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-output "aws_ami_id" {
-  value = data.aws_ami.latest-amazon-linux-image.id
-}
-
-resource "aws_key_pair" "ssh-key" {
-  key_name = "server-key"
-  public_key = file(var.public_key_location)
-}
-
-resource "aws_instance" "myapp-server" {
-  ami = data.aws_ami.latest-amazon-linux-image.id
+  my_ip = var.my_ip
+  env_prefix = var.env_prefix
+  public_key_location = var.public_key_location
   instance_type = var.instance_type
-
-  subnet_id = aws_subnet.myapp-subnet-1.id
-  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
-  availability_zone = var.avail_zone
-
-  associate_public_ip_address = true
-  key_name = aws_key_pair.ssh-key.key_name
-
-  user_data = file("entry-script.sh")
-
-  tags = {
-    Name = "${var.env_prefix}-server"
-  }
+  subnet_id = module.myapp-subnet.subnet.id
+  avail_zone = var.avail_zone
+  image_name = var.image_name
 }
 
-output "ec2_public_ip" {
-  value = aws_instance.myapp-server.public_ip
-}
+
+
