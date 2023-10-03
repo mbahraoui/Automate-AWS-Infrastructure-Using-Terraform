e7860e4dc2179e339e0453e2febd5d4fc7673f9e
mohamedbh <mohamedbahraoui20@gmail.com>
2023-10-03 11:29:49 +0100

    modularize tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_version = ">= 0.12"
+  backend "s3" {
+    bucket = "mbahraoui-myapp-bucket"
+    key = "myapp/state.tfstate"
+    region = "eu-west-3"
+  }
+}
+
 provider "aws" {
     region = "eu-west-3"
 }
